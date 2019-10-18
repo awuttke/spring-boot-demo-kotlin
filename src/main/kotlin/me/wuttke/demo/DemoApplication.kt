91a27a38292a6b257632686a3fc054efb3664a4a
Andreas Wuttke <awuttke@in-technology.eu>
2019-10-18 11:50:37 +0200

    Zugriff auf den ApplicationContext Definition und Verwendung einer Bean

--- src/main/kotlin/me/wuttke/demo/DemoApplication.kt
+++ src/main/kotlin/me/wuttke/demo/DemoApplication.kt
@@ -2,10 +2,19 @@ package me.wuttke.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.text.NumberFormat
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
 
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	fun main(args: Array<String>) {
+		runApplication<DemoApplication>(*args)
+	}
+
+	@Bean
+	fun defaultCurrencyFormat() : NumberFormat {
+		return NumberFormat.getCurrencyInstance()
+	}
 }
+
--- src/test/kotlin/me/wuttke/demo/DemoApplicationTests.kt
+++ src/test/kotlin/me/wuttke/demo/DemoApplicationTests.kt
@@ -2,15 +2,31 @@ package me.wuttke.demo
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.junit4.SpringRunner
+import java.text.NumberFormat
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	private lateinit var ctx: ApplicationContext
+
+	@Autowired
+	private lateinit var numberFormat: NumberFormat
+
+	@Test
+	fun defaultCurrency() {
+		val amount = 4711.15
+		println("Betrag: ${numberFormat.format(amount)}")
+	}
 
 	@Test
 	fun contextLoads() {
+		val count = ctx.beanDefinitionCount
+		println("Numer of Beans: $count")
 	}
 
 }
